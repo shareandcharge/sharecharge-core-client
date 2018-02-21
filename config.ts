2dab0092d8914ce93b45ac06d60687cd3cd274c4
stipa <stipa@motionwerk.com>
2018-02-21 14:23:53 +0100

    Add yaml parser for configuration

--- config.ts
+++ config.ts
@@ -1,11 +1,7 @@
-// import { Bridge } from './test/testBridge1';
-// import { Bridge } from './test/testBridge2';
-import { Bridge } from './src/bridge/src/bridge';
+import { Bridge } from './test/testBridge1';
 
 export const config = {
-    // id: '0x1234000000000000000000000000000000000000000000000000000000000000',
-    // pass: '',
-    test: false,
-    bridge: new Bridge(/*bridge has its own config for now*/),
-    statusUpdateInterval: 1000,
-};
+    test: true,
+    bridge: new Bridge(),
+    statusUpdateInterval: 2000,
+}
