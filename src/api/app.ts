bfecc6b990134086df79a504fa7a3b6efcf10710
Sebastian Gerske <sebastian.gerske@digitalunit.berlin>
2018-03-27 16:11:20 +0200

    moved to domain model

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,34 +1,34 @@
-import { ShareCharge, Wallet, Connector, Station, ToolKit, OpeningHours } from 'sharecharge-lib';
+import { ShareCharge, Wallet, Station, ToolKit, OpeningHours, Evse } from 'sharecharge-lib';
 import * as jwt from 'jsonwebtoken';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import { loadConfigFromFile } from '../utils/config';
-import { loggingProvider } from '../services/logger';
+import LoggingProvider from "../services/loggingProvider";
 
-// const config = loadConfigFromFile('./config/config.yaml');
-
+const logger = new LoggingProvider().obtain();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-export const config = {
-    stage: process.env.sc_stage || "local",
-    provider: process.env.sc_provider || 'http://localhost:8545',
-    gasPrice: 18000000000
-};
-
-const sc = new ShareCharge(config);
+const sc = ShareCharge.getInstance();
 const wallet = new Wallet('filter march urge naive sauce distance under copy payment slow just cool');
 
 sc.startListening();
-sc.on('StationCreated', result => { console.log(result); });
-sc.on('StationUpdated', result => { console.log(result); });
-sc.on('ConnectorCreated', result => { console.log(result); });
-sc.on('ConnectorUpdated', result => { console.log(result); });
+sc.on('StationCreated', result => {
+    console.log(result);
+});
+sc.on('StationUpdated', result => {
+    console.log(result);
+});
+sc.on('ConnectorCreated', result => {
+    console.log(result);
+});
+sc.on('ConnectorUpdated', result => {
+    console.log(result);
+});
 sc.on('StartRequested', result => {
     // if result.connectorId is one of ours since this will receive ALL messages
     // simulate delay of starting connector
     setTimeout(async () => {
-        const connector = await sc.connectors.getById(result.connectorId);
+        const connector = await sc.evses.getById(result.connectorId);
         sc.charging.useWallet(wallet).confirmStart(connector, result.controller);
     }, 500);
 });
@@ -36,16 +36,18 @@ sc.on('StopRequested', result => {
     // if result.connectorId is one of ours since this will receive ALL messages
     // simulate delay of starting connector
     setTimeout(async () => {
-        const connector = await sc.connectors.getById(result.connectorId);
+        const connector = await sc.evses.getById(result.connectorId);
         sc.charging.useWallet(wallet).confirmStop(connector, result.controller);
     }, 500);
 });
-sc.on('Error', result => { console.log("Error", result); });
+sc.on('Error', result => {
+    console.log("Error", result);
+});
 
 let to;
 
 app.use(bodyParser.json()); // support json bodies
-app.use(bodyParser.urlencoded({ extended: true }));  //support encoded bodies
+app.use(bodyParser.urlencoded({extended: true}));  //support encoded bodies
 
 export const listen = () => {
     app.listen(PORT, function () {
@@ -65,13 +67,13 @@ export const listen = () => {
 // get all stations
 app.get('/stations', verifyToken, async (req, res) => {
     const stations = await sc.stations.getAll();
-    res.send(stations.map(station => station.serialize()));
+    res.send(stations.map((station: Station) => Station.serialize(station)));
 });
 
 // get single station
 app.get('/stations/:id', verifyToken, async (req, res) => {
     const station = await sc.stations.getById(req.params.id);
-    res.send(station.serialize());
+    res.send(Station.serialize(station));
 });
 
 // create station
@@ -86,18 +88,17 @@ app.post('/stations', verifyToken, async (req, res) => {
 
 // create connector
 app.post('/connectors', verifyToken, async (req, res) => {
-    const connector = new Connector();
-    connector.owner = req.body.client || "";
+    const connector = new Evse();
     connector.plugTypes = ToolKit.fromPlugMask(req.body.plugTypes);
     connector.available = true;
-    await sc.connectors.useWallet(wallet).create(connector);
+    await sc.evses.useWallet(wallet).create(connector);
     res.send(connector.id);
 });
 
 // get informations for one connector
 app.get('/connectors/:id', verifyToken, async (req, res) => {
 
-    const connector = await sc.connectors.getById(req.params.id);
+    const connector = await sc.evses.getById(req.params.id);
     const station = await sc.stations.getById(connector.stationId);
 
     let response = {
@@ -124,30 +125,30 @@ app.get('/connectors/:id', verifyToken, async (req, res) => {
 
 //Disable the connector
 app.put('/disable/:id', verifyToken, async (req, res) => {
-    const connector = await sc.connectors.getById(req.params.id);
+    const connector = await sc.evses.getById(req.params.id);
     connector.available = false;
-    await sc.connectors.useWallet(wallet).update(connector);
+    await sc.evses.useWallet(wallet).update(connector);
     res.sendStatus(200);
 });
 
 //Enable the connector
 app.put('/enable/:id', verifyToken, async (req, res) => {
-    const connector = await sc.connectors.getById(req.params.id);
+    const connector = await sc.evses.getById(req.params.id);
     connector.available = true;
-    await sc.connectors.useWallet(wallet).update(connector);
+    await sc.evses.useWallet(wallet).update(connector);
     res.sendStatus(200);
 });
 
 //Request start
 app.put('/start/:id', verifyToken, async (req, res) => {
-    const connector = await sc.connectors.getById(req.params.id);
+    const connector = await sc.evses.getById(req.params.id);
     await sc.charging.useWallet(wallet).requestStart(connector, 10);
     res.send(200);
 });
 
 // Stop endpoint
 app.put('/stop/:id', verifyToken, async (req, res) => {
-    const connector = await sc.connectors.getById(req.params.id);
+    const connector = await sc.evses.getById(req.params.id);
     await sc.charging.useWallet(wallet).requestStop(connector);
     // clearTimeout(to);
     // const stop = await contract.sendTx('confirmStop', req.params.id);
@@ -186,10 +187,11 @@ jwt.sign({user: 'test'}, 'secretkey', {expiresIn: '20m'}, (err, token) => {
         console.log("Your json web token: ", token);
     }
 });
+
 // Verify Token
 function verifyToken(req, res, next) {
     // const bearerHeader = req.headers['authorization'];
-    
+
     // if (typeof bearerHeader === 'undefined') {
     //     res.sendStatus(403);
     // }
@@ -201,7 +203,7 @@ function verifyToken(req, res, next) {
     //     if (err) {
     //         res.sendStatus(403);
     //     } else {
-            next();
+    next();
     //     }
     // });
 }
