9816d7b0f909560399004d3070293094bd570eec
stipa <stipa@motionwerk.com>
2018-05-08 10:38:14 +0200

    add cdr filtering

--- src/cli/cpo/cdr.logic.ts
+++ src/cli/cpo/cdr.logic.ts
@@ -1,3 +1,4 @@
+import { log } from 'util';
 import { Evse, ShareCharge, ToolKit } from "@motionwerk/sharecharge-lib";
 import LogicBase from '../logicBase';
 
@@ -10,21 +11,76 @@ export default class CdrLogic extends LogicBase {
     public getInfo = async (argv) => {
         if (!argv.json) {
             this.client.logger.info("Displaying Charge Detail Record: ");
-            const cdrInfo = await this.getCDRInfo();            
+            const cdrInfo = await this.getCDRInfo(argv);            
             console.log(cdrInfo);
         }
     }
 
-    async getCDRInfo(): Promise<any> {
+    async getCDRInfo(argv): Promise<any> {
         const logDetails = await this.client.sc.charging.contract.getLogs('ChargeDetailRecord');
+        console.log(logDetails[0]);
+        let allLogs = logDetails.map(obj => (
+            {
+                transactionHash: obj.transactionHash,
+                address: obj.address,
+                controller: obj.returnValues.controller,
+                tokenAddress: obj.returnValues.tokenAddress,
+                date: new Date(obj.timestamp * 1000).toUTCString(),
+                timestamp: obj.timestamp
+                
+            }
+
+        ));
+
+        // filtering 
+        if(argv.transactionHash){ 
+            allLogs = allLogs.filter( key => (    
+                key.transactionHash === argv.transactionHash
+            ));
+            console.log(" Filtered by transactionHash: ");
+        } 
         
-        const cdrInfo = {
-            txHash: logDetails[0].transactionHash,
-            address: logDetails[0].address,
-            evseId: logDetails[0].returnValues.evseId,
-            controller: logDetails[0].returnValues.controller,
-            tokenAddress: logDetails[0].returnValues.tokenAddress
+        if(argv.address) {
+            allLogs = allLogs.filter( key => (
+                key.address === argv.address
+            ));
+            console.log(" Filtered by address: ");
+        } 
+        
+        if(argv.controller) {
+            allLogs = allLogs.filter( key => (
+                key.controller === argv.controller
+            ));
+            console.log(" Filtered by controller: ");
         }
-        return cdrInfo;
+
+        if(argv.evseId) {
+            allLogs = allLogs.filter( key => (
+                key.evseId === argv.evseId
+            ));
+            console.log(" Filtered by evseId: ");
+        }
+
+        if(argv.tokenAddress) {
+            allLogs = allLogs.filter( key => (
+                key.tokenAddress === argv.tokenAddress
+            ));
+            console.log(" Filtered by evseId: ");
+        }
+
+        if(argv.timestamp) {
+            allLogs = allLogs.filter( key => (
+                key.timestamp === argv.timestamp
+            ));
+            console.log(" Filtered by evseId: ");
+        }
+
+        if(argv.date) {
+            let date = new Date(argv.date).getTime() / 1000;
+            allLogs = allLogs.filter( key => (
+                key.timestamp >= date
+            ));
+        }
+        return allLogs;
     }
 }
