ec943c64b799af070fdbf60f79e95f8a167b6ade
adamstaveley <adam@motionwerk.com>
2018-06-13 16:23:39 +0200

    dist handled in ci and ci deployment

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const coreClient_1 = require("./coreClient");
-const client = coreClient_1.CoreClient.getInstance();
-client.run();
-// import '@motionwerk/sharecharge-api';
-//# sourceMappingURL=app.js.map
--- dist/app.node.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const coreClient_1 = require("./coreClient");
-const client = coreClient_1.CoreClient.getInstance();
-client.run();
-// import '@motionwerk/sharecharge-api';
-//# sourceMappingURL=app.js.map
