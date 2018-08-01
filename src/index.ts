a27a4dde6b6807ae2cfdb32819030cdf811a1cc3
adamstaveley <adam@motionwerk.com>
2018-08-01 12:32:03 +0200

    BREAKING: rename app.ts to index.ts; configurable API

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 
 import * as yargs from 'yargs';
 import { prepareConfigLocation, getConfigDir } from '@motionwerk/sharecharge-common';
+import api from '@motionwerk/sharecharge-api';
 import { CoreClient } from './coreClient';
 
 
@@ -13,12 +14,22 @@ yargs
             describe: 'Run the Share & Charge API',
             type: 'boolean',
         })
+        .option('api-host', {
+            describe: 'Configure API host to listen on',
+            type: 'string',
+            default: '127.0.0.1'
+        })
+        .option('api-port', {
+            describe: 'Configure API port to listen on',
+            type: 'number',
+            default: 3000
+        })
         // .default('no-api', u);
     }, (args: yargs.Arguments) => {
         const client = CoreClient.getInstance();
         client.main();
-        if (args['api']) {
-            require('@motionwerk/sharecharge-api');
+        if (args.api) {
+            api(args['api-host'], args['api-port']);
         }
     })
     
